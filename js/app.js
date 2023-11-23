67a7a51c6b662b74bb6158c369ca96464c3e9095
DaneschBa <106243588+DaneschBa@users.noreply.github.com>
2023-11-23 16:04:13 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -365,17 +365,3 @@ async function writeFile() {
   
   target.innerHTML = 'Test content written to ' + file.name + '.';
 }
-
-if ('getBattery' in navigator || ('battery' in navigator && 'Promise' in window)) {
-  var target = document.getElementById('target');
-
-  function handleChange(change) {
-    var timeBadge = new Date().toTimeString().split(' ')[0];
-    var newState = document.createElement('p');
-    newState.innerHTML = '' + timeBadge + ' ' + change + '.';
-    target.appendChild(newState);
-  }
-  
-  function onChargingChange() {
-    handleChange('Battery charging changed to ' + (this.charging ? 'charging' : 'discharging') + '')
-  }
