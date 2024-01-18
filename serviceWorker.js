943227a2746671ccc31da180274ccd50f9e0f5c0
DaneschBa <106243588+DaneschBa@users.noreply.github.com>
2024-01-18 17:52:24 +0100

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,14 +1,5 @@
 const staticDevCoffee = "dev-coffee-site-v1";
 
-
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets);
-    })
-  );
-});
-
 self.addEventListener("fetch", fetchEvent => {
   fetchEvent.respondWith(
     caches.match(fetchEvent.request).then(res => {
